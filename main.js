30ed347f045fef012aa3b281555f710426e23907
Student <student@RU-019.local>
2015-09-09 11:36:08 -0600

    something

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ console.log(victim)
 var victimName = prompt("What is the victim's name?")
 var victimPhone = prompt("What is the victim's phone number?")
 var victimStreet = prompt("What street does the victim live on?")
+
+for ( var i = 0; i > victim )

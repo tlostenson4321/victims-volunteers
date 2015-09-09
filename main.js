aee255dcec93b64ad2f925ce6c86c4fbd99ef5e0
Student <student@RU-019.local>
2015-09-09 12:13:09 -0600

    added more js

--- main.js
+++ main.js
@@ -5,4 +5,7 @@ var victimName = prompt("What is the victim's name?")
 var victimPhone = prompt("What is the victim's phone number?")
 var victimStreet = prompt("What street does the victim live on?")
 
-for ( var i = 0; i > victim )
+// for (var i = 0; var === prompt; i++) {
+// 	var victimName = prompt("What is the victim's name?")
+// }
+
---
+++ other-main.js
@@ -0,0 +1,8 @@
+
+var victim = prompt("How many disaster victims were there?")
+console.log(victim)
+
+var victimName = function(victim) {
+	while ()
+}
+// pretty sure need a while loop here

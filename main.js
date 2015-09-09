be472e822cd3e2e6d4de2791d10c4f4f733414bf
Student <student@RU-019.local>
2015-09-09 08:38:02 -0600

    added vars

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 var victim = prompt( "How many disaster victims were there?")
+console.log(victim)
+
+var victimName = prompt("What is the victim's name?")
+var victimPhone = prompt("What is the victim's phone number?")
+var victimStreet = prompt("What street does the victim live on?")
